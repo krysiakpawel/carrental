e45084f2453e53c16c12ecb6ca4a8251dc556587
Konrad Jan <konrad.wasilewski92@gmail.com>
2023-07-18 22:27:45 +0000

    18.07 Konrad test 2

--- src/main/java/org/rental/Main.java
+++ src/main/java/org/rental/Main.java
@@ -14,7 +14,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Test test");
-        System.out.println("8===)");
+        System.out.println("8===D");
 
     }
 }

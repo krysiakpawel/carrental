a15eb0d3412a83e14f68209c809adaed9ffc7582
krysiakpawel <krysiak.p@o2.pl>
2023-07-19 23:33:12 +0000

    18.08 Initial booking service functions implementation

--- src/main/java/org/rental/booking/BookingService.java
+++ src/main/java/org/rental/booking/BookingService.java
@@ -5,12 +5,51 @@ import org.rental.customer.Renter;
 import org.rental.extrases.Extras;
 import org.rental.price.Price;
 import org.rental.vehicle.Vehicle;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BookingService {
 
-    public Booking createNewBooking(Customer renter, Customer driver, Vehicle vehicle){
-        return new Booking(renter,driver,vehicle);
+    // add try & catch and LOGGER
+    // logic for checking that vehicle is not in more than one booking at the time
+
+    public void openBooking(Booking booking){
+
+        if(booking.getBookingWasOpened()){
+            System.out.println("LOG: Can not open Booking #" + booking.getBookingNumber() + ". Booking #" + booking.getBookingNumber() + " has been opened already");
+        } else {
+            booking.setStartingMileage(booking.getVehicle().getMileage());
+            booking.setBookingIsRunning(true);
+            booking.setBookingWasOpened(true);
+            System.out.println("LOG: Booking #" + booking.getBookingNumber() + " has been opened");
+        }
     }
+    public void closeBooking(Booking booking, int mileage){
+
+        if(booking.getBookingIsClosed()){
+            System.out.println("LOG: Can not close Booking #"  + booking.getBookingNumber() + ". Booking #" + booking.getBookingNumber() + " is already closed");
+        } else {
+            booking.setEndingMileage(Math.max(booking.getStartingMileage(),mileage));
+            booking.setBookingIsRunning(false);
+            booking.setBookingIsClosed(true);
+            booking.getVehicle().setMileage(Math.max(booking.getStartingMileage(),mileage));
+            System.out.println("LOG: Booking #" + booking.getBookingNumber() + " has been closed. Customer drove " + (booking.getEndingMileage() - booking.getStartingMileage()) + " km.");
+        }
+    }
+    public Booking createNewBooking(Customer renter, Customer driver, Vehicle vehicle) {
+        return new Booking(renter, driver, vehicle);
+    }
+
+
+
+
+
+
+
+
+
+
+
     public void modifyDates(Booking booking) {
     }
 
@@ -27,4 +66,6 @@ public class BookingService {
     }
 
     public void getNumberOfDays(){}
+
+    public void getBookingPrice(){}
 }
---
+++ src/main/java/org/rental/organization/employees/FrontDesk.java
@@ -0,0 +1,4 @@
+package org.rental.organization.employees;
+
+public class FrontDesk extends Employee{
+}

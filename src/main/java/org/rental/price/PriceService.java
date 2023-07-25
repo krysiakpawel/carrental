719114233a286ad09a299cdc7390c87f317404c5
krysiakpawel <krysiak.p@o2.pl>
2023-07-25 22:56:43 +0000

    25.07 Added 2 void methods

--- src/main/java/org/rental/price/PriceService.java
+++ src/main/java/org/rental/price/PriceService.java
@@ -1,4 +1,15 @@
 package org.rental.price;
 
+import org.rental.booking.Booking;
+
 public class PriceService {
+
+
+    public void getBookingPrice() {
+    }
+
+    public void modifyPrice(Booking booking, Price price) {
+
+
+    }
 }

da79c71ca1ba9034938934e8e0b38f23671b00a4
krysiakpawel <krysiak.p@o2.pl>
2023-08-01 00:04:49 +0000

    01.08 Repository methods added

--- src/main/java/org/rental/domain/booking/dao/BookingDao.java
+++ src/main/java/org/rental/domain/booking/dao/BookingDao.java
@@ -1,7 +1,15 @@
 package org.rental.domain.booking.dao;
 
+import jakarta.transaction.Transactional;
 import org.rental.domain.booking.Booking;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface BookingDao extends CrudRepository <Booking, Integer> {
+@Transactional
+@Repository
+public interface BookingDao extends CrudRepository <Booking, Long> {
+
+    Booking getBookingByBookingNumber(int bookingNumber);
+
+    Booking getBookingById(Long id);
 }
